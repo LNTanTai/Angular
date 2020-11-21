ec80e93140b221582c1a2a4e19ceb42641f82226
LNTanTai <71920398+LNTanTai@users.noreply.github.com>
2020-11-21 14:14:53 +0700

    Update app.module.ts

--- Angular_2/fe52-angular/src/app/app.module.ts
+++ Angular_2/fe52-angular/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { AppComponent } from './app.component';
 
 //ngModue: meta-data
 @NgModule({
-  //declarations: noi khai bao cac component duoc module quan ly
-  // => AppComponent dang duoc AppModule quan ly
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  // declarations: noi khai báo các component duoc module quan ly
+  // => AppComponent đang duoc AppModule quan ly
+  // Component ko the đứng 1 minh, no phai duoc 1 module quan ly
+  // 1 component chỉ co the duoc 1 module quan ly
+  declarations: [AppComponent],
+
+  // import: nơi khai báo các module
+  // HomeModule: HeaderComponent, FooterComponent, ContentComponent
+  // => De su dung duoc cac component cua HomeModule trong AppModule
+  // can phai import: [HomeModule]
+  // 2 loai module: 
+  // - Module do angular cung cap: RouterModule, HttpClientModule
+  // - Module do dev dinh nghia: HomeModule
+  imports: [BrowserModule],
+  
+  // Noi khai bao cac services
   providers: [],
+  
+  // Khai bao ApppComponent la component khoi chay dau tien
   bootstrap: [AppComponent]
 })
 export class AppModule { }
